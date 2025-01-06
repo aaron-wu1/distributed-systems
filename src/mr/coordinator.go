f2064a5b7997125c68b8154b9001e3e6db6eaf66
Aaron Wu <12989649+Aaron-Wu1@users.noreply.github.com>
2025-01-05 17:04:17 -0800

    lab1: added file rpc

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -1,33 +1,37 @@
 package mr
 
-import "log"
-import "net"
-import "os"
-import "net/rpc"
-import "net/http"
-
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"os"
+)
 
 type Coordinator struct {
 	// Your definitions here.
-
+	nReduce int
+	ch      chan string
 }
 
 // Your code here -- RPC handlers for the worker to call.
 
-//
 // an example RPC handler.
 //
 // the RPC argument and reply types are defined in rpc.go.
-//
 func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
 	reply.Y = args.X + 1
 	return nil
 }
 
+// Assign work
+func (c *Coordinator) FetchFile(args *FetchFileArgs, reply *FetchFileReply) error {
+	reply.File = <-c.ch
+	return nil
+}
 
-//
 // start a thread that listens for RPCs from worker.go
-//
 func (c *Coordinator) server() {
 	rpc.Register(c)
 	rpc.HandleHTTP()
@@ -41,29 +45,32 @@ func (c *Coordinator) server() {
 	go http.Serve(l, nil)
 }
 
-//
 // main/mrcoordinator.go calls Done() periodically to find out
 // if the entire job has finished.
-//
 func (c *Coordinator) Done() bool {
 	ret := false
 
 	// Your code here.
 
-
 	return ret
 }
 
-//
 // create a Coordinator.
 // main/mrcoordinator.go calls this function.
 // nReduce is the number of reduce tasks to use.
-//
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
-	c := Coordinator{}
+	c := Coordinator{
+		nReduce: nReduce,
+		ch:      make(chan string),
+	}
+	for _, file := range files {
+		go func(file string) {
+			c.ch <- file
+		}(file)
+	}
 
 	// Your code here.
-
+	fmt.Println(len(files), nReduce)
 
 	c.server()
 	return &c
